f85e2a069a0ba4c3fd96bb41e7bce2b37b1ccad7
melika <fatemeh.najafpour2018@gmail.com>
2024-10-26 12:05:19 +0330

    add some responsive in project

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -4,9 +4,9 @@ import { Link, useParams } from "react-router-dom";
 import { fetchProducts } from "../../Store/ProductSlice/ProductSlice"; // Ensure path is correct
 import { addToCart } from "../../Store/Cart/CartSlice"; // Ensure path is correct
 import "./ProductPage.css";
-import NavbarItem from "../navbarItem/NavbarItem";
+import NavbarItem from "../../Components/navbarItem/NavbarItem";
 import Footer from "../Footer/Footer";
-import Cart from "../Cart/Cart";
+import Cart from "../../Components/Cart/Cart";
 
 const ProductPage = () => {
   const dispatch = useDispatch();
@@ -41,10 +41,12 @@ const ProductPage = () => {
               <img src={product.image} alt={product.title} />
               <h6>{product.title}</h6>
               <p>Price: ${product.price}</p>
-              <p>{product.description}</p>
+              <p className="protext">{product.description}</p>
+              <div className="btncom">
               <button onClick={() => handleAddToCart(product)}>add to cart</button>
               <Link to="/" className="mx-3">back home</Link>
               <Link to="/product">Back ProductPage</Link>
+              </div>
             </div>
           </div>
         ) : (
